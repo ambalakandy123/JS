751e97eba73f18f16f9d4cc4f3f25a88b2a262c0
ambalakandy123 <ranjith.ra28@gmail.com>
2020-10-16 14:58:22 +1000

    JS
    basics again

--- sandbox.js
+++ sandbox.js
@@ -104,10 +104,51 @@
 //     console.log('you are still young and you can still make it')
 // }
 
-const password = 'p@sswardaa';
+// const password = 'p@ss';
 
-if(password.length <= 8){
-    console.log('that password is not long enough!');
+// if(password.length <= 2){
+//     console.log('that password is not long enough!');
+// }
+
+// else if(password.length >=8){
+//     console.log('that password is dope!')
+// }
+// else{
+//  console.log('the password is perfect');
+// }
+
+// const password = 'pasword';
+
+// if (password.length >= 12 && password.includes('@')){
+//     console.log('that password is mighty strong');
+// }
+// else if (password.length >=8 || password.includes('@') && password.length >=5){
+//     console.log('the password is ok')
+// }
+// else{
+//     console.log('passoword failed')
+// }
+
+// let user = false;
+// if(!user){
+// console.log("you must log in for the data!")
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+const scores =[50, 25, 0, 30, 100, 20, 10]
+
+for(let i = 0; i<scores.length; i++){
+
+    if(scores[i] === 0 ){
+        continue; 
+    }
+
+    console.log('your score: ', scores[i]);
+
+    if (scores[i] === 100){
+        console.log('congrats you scored well')
+        break;
+    }
 }
-else
- console.log('the password is perfect');
