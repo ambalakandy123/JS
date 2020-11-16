179eeb0c66f40634d6fd7be49f1d46f9ddfd96db
ambalakandy123 <ranjith.ra28@gmail.com>
2020-11-16 13:09:59 +1000

    DOM
    setting attributes, removing classes, parent, children and sibilings, click Listener.

--- sandboxV2.js
+++ sandboxV2.js
@@ -28,12 +28,76 @@
 //     para.innerText += ' new text';
 // });
 
-const content = document.querySelector('.content');
+// const content = document.querySelector('.content');
 // console.log(content.innerHTML);
 // content.innerHTML = '<h2> This is a new sub-heading</>'
 
-const people = ['yoko', 'zochi', 'pakiao'];
+// const people = ['yoko', 'zochi', 'pakiao'];
 
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`
-})
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`
+// })
+
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href','creativeMinds.com');
+// link.innerText = 'The creative website';
+
+// const title = document.querySelector('h1');
+// title.setAttribute('style', 'margin:50px;');
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = '80px';
+// title.style.color ='green';
+
+// console.log(title.style.color);
+
+// const content = document.querySelector('p');
+// console.log(content.classList);
+
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(p =>{
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error');
+//     }
+//     if(p.innerText.includes('success')){
+//         p.classList.add('success');
+//     }
+// });
+
+// const title = document.querySelector('.title');
+// title.classList.toggle('test');
+// title.classList.toggle('test');
+
+// const article = document.querySelector('article')
+// console.log(article.children);
+// console.log(Array.from(article.children));
+// console.log(article.children);
+
+// Array.from(article.children).forEach(child => {
+//     child.classList.add('article-element');
+// });
+
+// const title = document.querySelector('h2');
+// console.log(title.parentElement);
+// console.log(title.parentElement.parentElement);
+// console.log(title.nextElementSibling);
+// console.log(title.previousElementSibling);
+//chaining
+// console.log(title.nextElementSibling.parentElement.children);
+
+// const button = document.querySelector('button');
+// button.addEventListener('click', ()=>{
+//     console.log('you clicked the hell out of me!')
+// }) 
+
+// const items = document.querySelectorAll('li');
+
+// items.forEach(item => {
+//     item.addEventListener('click', () =>{
+//         console.log('item clicked');
+//     })
+    
+// });
